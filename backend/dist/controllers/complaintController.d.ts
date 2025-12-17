28adc075c08021c2b6482c3cef643ae6202ab348
kaveyan18 <kaveyan005@gmail.com>
2025-12-17 23:12:49 +0530

    final

--- backend/dist/controllers/complaintController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-export declare const createComplaint: (req: Request, res: Response) => Promise<void>;
-export declare const getComplaints: (req: Request, res: Response) => Promise<void>;
-export declare const getComplaintById: (req: Request, res: Response) => Promise<void>;
-export declare const updateComplaint: (req: Request, res: Response) => Promise<void>;
-export declare const deleteComplaint: (req: Request, res: Response) => Promise<void>;
-export declare const getComplaintStats: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=complaintController.d.ts.map
--- backend/dist/controllers/userController.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-export declare const registerUser: (req: Request, res: Response) => Promise<void>;
-export declare const loginUser: (req: Request, res: Response) => Promise<void>;
-export declare const getUserById: (req: Request, res: Response) => Promise<void>;
-export declare const getStaffMembers: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=userController.d.ts.map
